888a631627d1909f5007e442640acaa63106442e
kajusviliusis <kajus.viliusis0@gmail.com>
2026-01-30 19:04:25 +0200

    feat: ticket entity

---
+++ src/main/java/org/example/pulsedesk/model/Ticket.java
@@ -0,0 +1,28 @@
+package org.example.pulsedesk.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Ticket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    private String title;
+    private String category;
+    private String priority;
+    private String summary;
+
+    private LocalDateTime createdAt =  LocalDateTime.now();
+}
