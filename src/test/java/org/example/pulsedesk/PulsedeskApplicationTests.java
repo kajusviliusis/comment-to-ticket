cd8652dddc89de0ba426bf0f42db9282b8c4706d
kajusviliusis <kajus.viliusis0@gmail.com>
2026-01-30 18:27:29 +0200

    chore: initialize project

---
+++ src/test/java/org/example/pulsedesk/PulsedeskApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pulsedesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulsedeskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
